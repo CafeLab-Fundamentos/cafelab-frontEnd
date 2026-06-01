f54f39da2964ce89689048822b407368cc0db780
yigabriel <gabrielsacc7@gmail.com>
2026-05-31 19:47:12 -0500

    feat: i18n

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: 'http://localhost:8080',
+  serverBaseUrl: 'https://cafelab-api-gateway-gnfua0csgsbud3eh.canadacentral-01.azurewebsites.net',
   coffeeLotsEndpointPath: '/api/v1/coffee-lots',
   suppliersEndpointPath: '/api/v1/suppliers',
   roastProfileEndpointPath: '/api/v1/roast-profiles',
